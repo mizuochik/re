b7ce77d452d2ef2fff3ed32af4c011cfc8dcadfc
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-04-27 00:31:12 +0900

    Refactoring: Extract editor

---
+++ editor/editor.go
@@ -0,0 +1,74 @@
+package editor
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"syscall"
+
+	"github.com/pkg/term/termios"
+	"golang.org/x/sys/unix"
+)
+
+type Editor struct {
+	OriginalTermios *unix.Termios
+}
+
+func (e *Editor) SetRawMode() {
+	var orig unix.Termios
+	if err := termios.Tcgetattr(0, &orig); err != nil {
+		panic(err)
+	}
+	t := orig
+	t.Iflag &^= syscall.BRKINT | syscall.ICRNL | syscall.INPCK | syscall.ISTRIP | syscall.IXON
+	t.Oflag &^= syscall.OPOST
+	t.Cflag |= syscall.CS8
+	t.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.IEXTEN | syscall.ISIG
+	t.Cc[unix.VMIN] = 0
+	t.Cc[unix.VTIME] = 1
+	termios.Tcsetattr(0, unix.TCIFLUSH, &t)
+	e.OriginalTermios = &orig
+}
+
+func (e *Editor) ResetRawMode() {
+	termios.Tcsetattr(0, unix.TCIFLUSH, e.OriginalTermios)
+}
+
+func (e *Editor) RefreshScreen() {
+	fmt.Print("\x1b[2J")
+	fmt.Print("\x1b[H")
+}
+
+func (e *Editor) DrawRaws() {
+	for i := 0; i < 24; i++ {
+		fmt.Print("~\r\n")
+	}
+}
+
+func (e *Editor) HandleKey(k rune) error {
+	fmt.Printf("%d (%c) ", k, k)
+	return nil
+}
+
+func (e *Editor) ReadKey(ctx context.Context) chan rune {
+	c := make(chan rune)
+	go func() {
+		<-ctx.Done()
+		close(c)
+	}()
+	go func() {
+		rd := bufio.NewReader(os.Stdin)
+		for {
+			r, _, err := rd.ReadRune()
+			if err != nil {
+				if err.Error() == "EOF" {
+					continue // timeout
+				}
+				panic(err)
+			}
+			c <- r
+		}
+	}()
+	return c
+}
--- main.go
+++ main.go
@@ -1,85 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"os"
 	"os/signal"
 	"syscall"
 	"unicode"
 
-	"github.com/pkg/term/termios"
-	"golang.org/x/sys/unix"
+	"github.com/mizuochikeita/re/editor"
 )
-
-func ReadKey(ctx context.Context) chan rune {
-	c := make(chan rune)
-	go func() {
-		<-ctx.Done()
-		close(c)
-	}()
-	go func() {
-		rd := bufio.NewReader(os.Stdin)
-		for {
-			r, _, err := rd.ReadRune()
-			if err != nil {
-				if err.Error() == "EOF" {
-					continue // timeout
-				}
-				panic(err)
-			}
-			c <- r
-		}
-	}()
-	return c
-}
-
-func SetRawMode() unix.Termios {
-	var orig unix.Termios
-	if err := termios.Tcgetattr(0, &orig); err != nil {
-		panic(err)
-	}
-	t := orig
-	t.Iflag &^= syscall.BRKINT | syscall.ICRNL | syscall.INPCK | syscall.ISTRIP | syscall.IXON
-	t.Oflag &^= syscall.OPOST
-	t.Cflag |= syscall.CS8
-	t.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.IEXTEN | syscall.ISIG
-	t.Cc[unix.VMIN] = 0
-	t.Cc[unix.VTIME] = 1
-	termios.Tcsetattr(0, unix.TCIFLUSH, &t)
-	return orig
-}
-
-func ResetRawMode(orig unix.Termios) {
-	termios.Tcsetattr(0, unix.TCIFLUSH, &orig)
-}
-
-func HandleKey(k rune) error {
-	fmt.Printf("%d (%c) ", k, k)
-	return nil
-}
-
-func RefreshScreen() {
-	fmt.Print("\x1b[2J")
-	fmt.Print("\x1b[H")
-}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-	defer ResetRawMode(SetRawMode())
-	RefreshScreen()
-	for k := range ReadKey(ctx) {
+
+	e := &editor.Editor{}
+
+	e.SetRawMode()
+	defer e.ResetRawMode()
+
+	e.RefreshScreen()
+	e.DrawRaws()
+
+	for k := range e.ReadKey(ctx) {
 		switch {
 		case unicode.IsControl(k):
 			continue
 		case k == 'q':
-			RefreshScreen()
+			e.RefreshScreen()
 			cancel()
 		default:
-			HandleKey(k)
+			e.HandleKey(k)
 		}
 	}
 }
