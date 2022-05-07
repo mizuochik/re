2c06fc73cea32c011b91eed2222bbb63353dc00b
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-08 00:31:54 +0900

    Add a debug util

--- editor/util.go
+++ editor/util.go
@@ -1,5 +1,20 @@
 package editor
 
+import (
+	"fmt"
+	"os"
+)
+
 func ToControl(r rune) rune {
 	return r & 0x1f
+}
+
+func Debugf(format string, a ...interface{}) {
+	f, err := os.OpenFile("/tmp/re.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0700)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fmt.Fprintf(f, format, a...)
+	fmt.Fprintln(f)
 }
